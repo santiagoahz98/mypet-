63f0f1c42589f3fa985ddc82508308666127457e
Daniela2512 <danivillarbol@gmail.com>
2021-08-28 10:01:05 -0500

    Descripcion

---
+++ demoDVA/src/main/java/com/DVA/DemoDvaApplication.java
@@ -0,0 +1,13 @@
+package com.DVA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDvaApplication.class, args);
+	}
+
+}
---
+++ demoDVA/src/test/java/com/DVA/DemoDvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DVA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
