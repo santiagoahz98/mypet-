b31b6718a7497a6f7538329638b13b36a6f4435e
santiagoahz98 <sahz1998@hotmail.com>
2021-09-04 11:05:52 -0500

    Commit 3

--- demoSAH/src/main/java/com/SAH/entity/Products.java
+++ demoSAH/src/main/java/com/SAH/entity/Products.java
@@ -1,4 +1,4 @@
-package com.SAH;
+package com.SAH.entity;
 import java.io.Serializable;
 public class Products implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -7,6 +7,7 @@ public class Products implements Serializable {
 	private String product_description;
 	private int product_stock;
 	private int product_price;
+	private long id_pet;
 	
 	public long getId() {
 		return id;
@@ -38,20 +39,29 @@ public class Products implements Serializable {
 	public void setProduct_price(int product_price) {
 		this.product_price = product_price;
 	}
+	public long getId_pet() {
+		return id_pet;
+	}
+	public void setId_pet(long id_pet) {
+		this.id_pet = id_pet;
+	}
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-	//@Override
-	//public String toString() {
-	//	return "Products [id=" + id + ", product_name= " + product_name + ", product_description=" + product_description + ",product_stock=" + product_stock + ",product_price=" + product_price + " ];
-	//}
-	public Products(long id, String product_name, String product_description, int product_stock, int product_price) {
+	@Override
+	public String toString() {
+		return "Products [id=" + id + ", product_name= " + product_name + ", product_description=" + product_description + ",product_stock=" + product_stock + ",product_price=" + product_price + ",id_pet=" + id_pet + " ] ";
+	}
+	public Products(long id, String product_name, String product_description, int product_stock, int product_price,
+			long id_pet) {
 		super();
 		this.id = id;
 		this.product_name = product_name;
 		this.product_description = product_description;
 		this.product_stock = product_stock;
 		this.product_price = product_price;
+		this.id_pet = id_pet;
 	}
+	
 	
 }
